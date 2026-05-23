dc6e5e00649c1c03787d5e8be33433e23ecc5805
andreacardenas0610 <andrea.cardenas0610@gmail.com>
2026-05-23 11:21:27 -0500

    fix: optimización de comando build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1600, // Evita alertas por tamaño de código
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignora las advertencias que rompen el build en Vercel
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+        warn(warning);
+      },
+    },
+  },
+});
